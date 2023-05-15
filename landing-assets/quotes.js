a1dcd10b694a5a5c333856cf60ef8d02772e3378
ThatStella7922 <10524728+ThatStella7922@users.noreply.github.com>
2023-05-15 17:18:50 -0600

    Modify quotes

--- landing-assets/quotes.js
+++ landing-assets/quotes.js
@@ -12,13 +12,12 @@ const quotes = [
     "Let me forget everything, still moonlight shines on us<br>Broken heart, I don't want you to find and take a look at<br>I swear on the darkest night I'll end it all, <strong>and Testify</strong>", // from Testify by Mournfinale
     "Knowledge may give weight, but accomplishments give lustre, and many more people see than weigh", // from Philip Stanhope
     "Somewhere, someone is still thinking about someone else<br>Even in times of change what matters, will last forever", // from God Sees All by Ethyria
-    "You don't know how broken someone may be, so be kind", // from myself
-    "There's no more night, blue skies forever<br>'Cause we're the masters of our own fate<br>We're the captains of our own souls", // from Lust for Life by Lana Del Rey
     "The secret of change is to focus all of your energy.<br>Not on fighting the old, but on building the new", // from Way of the Peaceful Warrior: A Book that Changes Lives by Dan Millman
     "So come, take my hand and<br>Come, make me feel what it's like<br>When love comes alive", // from Circles by KIRA
     "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind", // from Bernard M. Baruch
     "Another day, another dream?<br>Either way, make the most of it", // from myself
     "The only good is knowledge<br>And the only evil is ignorance", // from Socrates
+    "Indecisiveness and hesitation.<br>Avoid them both or you will always be filled with regret", // from myself
 ];
 
 function setQuotesText() {
