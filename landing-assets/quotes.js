410bcc731ab299ce351afae3b297f4d13fff5eff
Stella <ThatStella7922@users.noreply.github.com>
2023-06-16 23:46:12 -0600

    special quote message WIP

--- landing-assets/quotes.js
+++ landing-assets/quotes.js
@@ -9,9 +9,6 @@ const quotes = [
     "You've shown me light<br>You gave me wings to fly<br>And I will always be thankful", // from Butterfly by kmac2021
     "Stars can't shine without darkness", // from Fragile Truths by D.H. Sidebottom
     "Sometimes you put walls up not to keep people out,<br>but to see who cares enough to break them down", // from Socrates
-    "Let me forget everything, still moonlight shines on us<br>Broken heart, I don't want you to find and take a look at<br>I swear on the darkest night I'll end it all, <strong>and Testify</strong>", // from Testify by Mournfinale
-    "Knowledge may give weight, but accomplishments give lustre, and many more people see than weigh", // from Philip Stanhope
-    "Somewhere, someone is still thinking about someone else<br>Even in times of change what matters, will last forever", // from God Sees All by Ethyria
     "The secret of change is to focus all of your energy.<br>Not on fighting the old, but on building the new", // from Way of the Peaceful Warrior: A Book that Changes Lives by Dan Millman
     "So come, take my hand and<br>Come, make me feel what it's like<br>When love comes alive", // from Circles by KIRA
     "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind", // from Bernard M. Baruch
@@ -21,6 +18,22 @@ const quotes = [
 ];
 
 function setQuotesText() {
+    const currentFullDate = new Date();
+    const currentMonth = currentFullDate.getMonth() + 1;
+    console.log(currentMonth)
+  
+    switch (currentMonth) {
+      case 6:
+        var randomText = "Happy Pride Month! 🏳️‍🌈🏳️‍⚧️"
+      
+      default:
+        var randomText = quotes[~~(Math.random() * quotes.length)];
+    }
+
+    console.log(randomText);
+}
+
+function setQuotesTextTemp() {
     var randomText = quotes[~~(Math.random() * quotes.length)];
     document.getElementById('quotep').innerHTML = randomText;
 }
