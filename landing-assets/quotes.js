e62dae5eb6fc7d51a329ee8d4075b17245e27110
ThatStella7922 <10524728+ThatStella7922@users.noreply.github.com>
2023-08-22 01:01:36 -0600

    Remove <br> from some quotes

--- landing-assets/quotes.js
+++ landing-assets/quotes.js
@@ -2,7 +2,7 @@
 ThatStella7922 Landing Website "quotes" Handler
 No I didn't have a better name for it
 
-v2023.0816.0
+v2023.0822.0
 */
 
 // Globals
@@ -15,14 +15,14 @@ var currentDay = currentFullDate.getDate();
 const quotes = [
     "You've shown me light<br>You gave me wings to fly<br>And I will always be thankful", // from Butterfly by kmac2021
     "Stars can't shine without darkness", // from Fragile Truths by D.H. Sidebottom
-    "Sometimes you put walls up not to keep people out,<br>but to see who cares enough to break them down", // from Socrates
+    "Sometimes you put walls up not to keep people out, but to see who cares enough to break them down", // from Socrates
     "The secret of change is to focus all of your energy.<br>Not on fighting the old, but on building the new", // from Way of the Peaceful Warrior: A Book that Changes Lives by Dan Millman
     "So come, take my hand and<br>Come, make me feel what it's like<br>When love comes alive", // from Circles by KIRA
     "If there are any gods, they must be jealous of the beauty and significance that mortality brings to every one of our cherished moments", // from myself
     "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind", // from Bernard M. Baruch
-    "Another day, another dream?<br>Either way, make the most of it", // from myself
+    "Another day, another dream? Either way, make the most of it", // from myself
     "The only good is knowledge<br>And the only evil is ignorance", // from Socrates
-    "Indecisiveness and hesitation.<br>Avoid them both or you will always be filled with regret", // from myself
+    "Indecisiveness and hesitation. Avoid them both or you will always be filled with regret", // from myself
 ];
 
 function getRandomQuote() {
