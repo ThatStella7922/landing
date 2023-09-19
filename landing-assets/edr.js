fae8b84b18be5fc4b03058ba3d7fa275919cb773
ThatStella7922 <10524728+ThatStella7922@users.noreply.github.com>
2023-09-18 23:33:27 -0400

    Clean up EDR stuff - Rename XDR to EDR to be more accurate - Ensure that the EDR stuff only shows on supported configurations - uhhh maybe something else, i forger

---
+++ landing-assets/edr.js
@@ -0,0 +1,25 @@
+/* 
+ThatStella7922 Landing Website EDR Handler
+
+This doesn't actually do anything useful, it just has a function for blinding the user lmfao
+
+v2023.0918.1
+*/
+
+function promptSetMaxEDRBrightness() {
+  edrBrightnessPrompt = confirm("Enable maximum EDR brightness for all elements on the page? The page already has some subtle EDR elements!\nOn capable displays, this will be extremely bright. Otherwise, it'll clip to white.\nAdditionally, the EDR stuff is only supported on WebKit-based browsers like Safari.");
+  if (edrBrightnessPrompt) {
+    // set max xdr on everything
+    document.getElementById("verticalcontainer").className = "edr-max";
+    
+    // unset xdr on a few elements so they're not dimmer
+    document.getElementById("textabovelinks").className = "";
+    document.getElementById("navigation-buttons").className = "links";
+    document.getElementById("social-link-buttons-id").className = "social-link-buttons";
+    document.getElementById("about-heading").className = "social-link-buttons";
+
+    // disable the button
+    document.getElementById("edr-enable").onclick = null;
+    document.getElementById('edr-enable').innerHTML = "<strong>EDR maxed!</strong>";
+  }
+}
